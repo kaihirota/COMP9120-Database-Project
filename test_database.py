4faf54897fa8b4c9784eb0b95b9c98c7787f270b
Kai Hirota <kai@khirota.co>
2020-04-28 16:57:17 +1000

    revert test_database

--- test_database.py
+++ test_database.py
@@ -6,22 +6,4 @@ with open(f'SQL_CREDENTIALS.json') as creds_file:
 
 
 def test_connection():
-    conn = connect(database='comp9120_a2', **sql_creds)
-
-    if not conn:
-        print('connection failed')
-    else:
-        return conn
-
-
-conn = test_connection()
-cursor = conn.cursor()
-sql = """
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_schema = 'public'
-"""
-
-cursor.execute(sql)
-for table in cursor.fetchall():
-    print(table)
+    connect(database='comp9120_a2', **sql_creds)
