9331eabcb6f13a0339d89f49df7b314e20e63e58
Kai Hirota <kai@khirota.co>
2020-04-28 23:49:22 +1000

    finished DDL and first draft of dummy_insert

--- ddl.sql
+++ ddl.sql
@@ -1,37 +1,3 @@
--- example
-CREATE TABLE Example(
-    staffid INTEGER PRIMARY KEY,
-    position VARCHAR(20) NOT NULL,
-    name INTEGER DEFAULT 1,
-    birthday DATE NULL,
-    age INTEGER CHECK( age >= 0 AND age < 150),
-    country VARCHAR(20),
-    -- if composite PK
-    PRIMARY KEY (sid,uos),
-    FOREIGN KEY (sid) REFERENCES Student,
-
-    -- the on delete cascade conveys
-    -- that an enrolled row should be
-    -- deleted when the student with sid
-    -- that it refers to is deleted
-    ON DELETE CASCADE
-
-    -- the on update set default
-    -- will attempt to update the
-    -- value of sid to a default value
-    -- that is specified as the default
-    -- in this Enrolled schema definition
-    ON UPDATE SET DEFAULT
-
-    REFERENCES Student ON DELETE CASCADE,
-    REFERENCES UnitOfStudy ON DELETE CASCADE,
-
-    CONSTRAINT PK_enrolled PRIMARY KEY (sid,uos)
-    CONSTRAINT FK_sid_enrolled FOREIGN KEY (sid)
-    CONSTRAINT CK_grade_enrolled CHECK(grade IN ('F', 'A')),
-);
--- constraints: UNIQUE, NOT NULL, UNIQUE, DEFAULT, CHECK,
-
 CREATE TABLE Staff(
     staffid INTEGER PRIMARY KEY,
     position VARCHAR(20),
@@ -49,44 +15,18 @@ CREATE TABLE MenuItem(
     itemType VARCHAR(10) NOT NULL,
     CONSTRAINT CK_itemType_MenuItem CHECK(itemType IN ('Main', 'Side', 'Dessert'))
 );
-CREATE TABLE MenuContains(
-    menuId INTEGER,
-    menuItemId INTEGER,
-    PRIMARY KEY (menuId, menuItemId),
-    CONSTRAINT FK_menuId_MenuContains FOREIGN KEY (menuId) REFERENCES Menu,
-    CONSTRAINT FK_menuItemId_MenuContains FOREIGN KEY (menuItemId) REFERENCES MenuItem
-);
-CREATE TABLE OrderItem(
-    orderItemId INTEGER PRIMARY KEY,
-    orderId INTEGER NOT NULL,
-    customerId INTEGER NOT NULL,
-    menuItemId INTEGER NOT NULL,
-    quantity INTEGER NOT NULL,
-    charge FLOAT NOT NULL,
-    CONSTRAINT FK_orderId_OrderItem FOREIGN KEY (orderId) REFERENCES Order,
-    CONSTRAINT FK_customerId_OrderItem FOREIGN KEY (customerId) REFERENCES Customer,
-    CONSTRAINT FK_menuItemId_OrderItem FOREIGN KEY (menuItemId) REFERENCES MenuItem,
-    CONSTRAINT CK_quantity_OrderItem CHECK(quantity > 0)
-);
-CREATE TABLE Order(
-    orderId INTEGER PRIMARY KEY,
-    customerId INTEGER NOT NULL,
-    staffId INTEGER NOT NULL,
-    deliveryId INTEGER NOT NULL,
-    -- using reserved words like DATETIME to name columns is bad practice
-    dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    totalCharge FLOAT NOT NULL,
-    CONSTRAINT FK_customerId_Order FOREIGN KEY (customerId) REFERENCES Customer,
-    CONSTRAINT FK_staffId_Order FOREIGN KEY (staffId) REFERENCES Staff,
-    CONSTRAINT FK_deliveryId_Order FOREIGN KEY (deliveryId) REFERENCES Delivery,
-    CONSTRAINT CK_totalCharge_Order CHECK(totalCharge > 0)
-);
 CREATE TABLE Customer(
     customerId INTEGER PRIMARY KEY,
     firstName VARCHAR(30) NOT NULL,
     lastName VARCHAR(30) NOT NULL,
     address VARCHAR(100) NOT NULL,
-    mobileNumber INTEGER NOT NULL
+    mobileNumber VARCHAR(10) NOT NULL
+);
+CREATE TABLE Courier(
+    courierId INTEGER PRIMARY KEY,
+    name VARCHAR(20) NOT NULL,
+    address VARCHAR(100), -- NOT NULL?
+    mobileNumber VARCHAR(10) -- NOT NULL?
 );
 CREATE TABLE Delivery(
     deliveryId INTEGER PRIMARY KEY,
@@ -96,9 +36,37 @@ CREATE TABLE Delivery(
     CONSTRAINT FK_courierId_Delivery FOREIGN KEY (courierId) REFERENCES Courier,
     CONSTRAINT CK_timeConflict_Delivery CHECK(timeReady < timeDelivered)
 );
-CREATE TABLE Courier(
-    courierId INTEGER PRIMARY KEY,
-    name VARCHAR(20) NOT NULL,
-    address VARCHAR(100), -- NOT NULL?
-    mobileNumber INTEGER -- NOT NULL?
+CREATE TABLE MenuContains(
+    menuId INTEGER,
+    menuItemId INTEGER,
+    PRIMARY KEY (menuId, menuItemId),
+    CONSTRAINT FK_menuId_MenuContains FOREIGN KEY (menuId) REFERENCES Menu ON DELETE CASCADE,
+    CONSTRAINT FK_menuItemId_MenuContains FOREIGN KEY (menuItemId) REFERENCES MenuItem ON DELETE CASCADE
+);
+CREATE TABLE "Order"(
+    orderId INTEGER PRIMARY KEY,
+    customerId INTEGER NOT NULL,
+    staffId INTEGER NOT NULL,
+    deliveryId INTEGER NOT NULL,
+    -- using reserved words like DATETIME to name columns is bad practice
+    dt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    totalCharge FLOAT NOT NULL,
+    CONSTRAINT FK_customerId_Order FOREIGN KEY (customerId) REFERENCES Customer ON DELETE CASCADE,
+    CONSTRAINT FK_staffId_Order FOREIGN KEY (staffId) REFERENCES Staff, -- ON DELETE CASCADE?
+    CONSTRAINT FK_deliveryId_Order FOREIGN KEY (deliveryId) REFERENCES Delivery ON DELETE CASCADE, -- necessary?
+    CONSTRAINT CK_totalCharge_Order CHECK(totalCharge > 0)
+);
+CREATE TABLE OrderItem(
+    orderItemId INTEGER PRIMARY KEY,
+    orderId INTEGER NOT NULL,
+    customerId INTEGER NOT NULL,
+    menuItemId INTEGER NOT NULL,
+    quantity INTEGER NOT NULL,
+    charge FLOAT NOT NULL,
+    CONSTRAINT FK_orderId_OrderItem FOREIGN KEY (orderId) REFERENCES "Order" ON DELETE CASCADE,
+    CONSTRAINT FK_customerId_OrderItem FOREIGN KEY (customerId) REFERENCES Customer, -- ON DELETE CASCADE?
+    CONSTRAINT FK_menuItemId_OrderItem FOREIGN KEY (menuItemId) REFERENCES MenuItem, -- ON DELETE CASCADE?
+    CONSTRAINT CK_quantity_OrderItem CHECK(quantity > 0)
+    -- add check - ensure order and order item matches
+    -- what about item price and order match? check that too?
 );
---
+++ dummy_insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO Staff(staffid, position, name) VALUES (1, 'General Manager', 'Generic Name');
+INSERT INTO Menu(menuId, description) VALUES (1, 'Basic Menu');
+INSERT INTO MenuItem(menuItemId, name, price, description, itemType) VALUES (1, 'Air', 100.00, 'Organic and fresh', 'Main');
+INSERT INTO Customer(customerId, firstName, lastName, address, mobileNumber) VALUES (1, 'Average', 'Joe', 'Not Earth', '0456123456');
+INSERT INTO Courier(courierId, name, address, mobileNumber) VALUES (1, 'TheFirstCoutier', 'Generic Address of Courier', '0432123456');
+INSERT INTO Delivery(deliveryId, courierId, timeReady, timeDelivered) VALUES (1, 1, CURRENT_TIMESTAMP - interval '2 hour', CURRENT_TIMESTAMP);
+INSERT INTO MenuContains(menuId, menuItemId) VALUES (1, 1);
+INSERT INTO "Order"(orderId, customerId, staffId, deliveryId, totalCharge) VALUES (1, 1, 1, 1, 112.98);
+INSERT INTO OrderItem(orderItemId, orderId, customerId, menuItemId, quantity, charge) VALUES (1, 1, 1, 1, 2, 100);
